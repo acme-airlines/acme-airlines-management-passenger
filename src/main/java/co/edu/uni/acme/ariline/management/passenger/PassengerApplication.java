4634824882d39033281b5abf4a4333bafae65d13
MariaFernanda1818 <mariafvn0127@gmail.com>
2025-03-30 09:54:22 -0500

    Se crea base del passenger

---
+++ src/main/java/co/edu/uni/acme/ariline/management/passenger/PassengerApplication.java
@@ -0,0 +1,19 @@
+package co.edu.uni.acme.ariline.management.passenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication(scanBasePackages = "co.edu.uni.acme.aerolinea")
+@EntityScan("co.edu.uni.acme.aerolinea")
+@ComponentScan(basePackages = {"co.edu.uni.acme.aerolinea"})
+@Import(ResourceServerConfig.class)
+public class PassengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerApplication.class, args);
+	}
+
+}
