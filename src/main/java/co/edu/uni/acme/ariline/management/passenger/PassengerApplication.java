5698a6a6921c88e09b6a329e16df354ec65a5fd9
MariaFernanda1818 <mariafvn0127@gmail.com>
2025-04-05 21:31:01 -0500

    Se ajusta main, pom  y yml

--- src/main/java/co/edu/uni/acme/ariline/management/passenger/PassengerApplication.java
+++ src/main/java/co/edu/uni/acme/ariline/management/passenger/PassengerApplication.java
@@ -3,6 +3,7 @@ package co.edu.uni.acme.ariline.management.passenger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
@@ -15,7 +16,9 @@ import co.edu.uni.acme.aerolinea.commons.configuration.ResourceServerConfig;
 public class PassengerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PassengerApplication.class, args);
+		new SpringApplicationBuilder(PassengerApplication.class)
+				.properties("spring.config.name=application")
+				.properties("spring.config.location=classpath:/application.properties") // <-- Aquí forzas que use solo ese
+				.run(args);
 	}
-
 }
